64601bb19d518b26e8cd5722359e42219e715e55
Greg Cook <greg@xerris.com>
2019-12-11 14:23:16 -0700

    json extentions addition

--- src/Xerris.DotNet.Core/Extensions/JsonExtensions.cs
+++ src/Xerris.DotNet.Core/Extensions/JsonExtensions.cs
@@ -1,14 +1,31 @@
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Xerris.DotNet.Core.Extensions
 {
     public static class JsonExtensions
     {
-        public static string ToJson<T>(this T item)
+        private static readonly DefaultContractResolver ContractResolver = new DefaultContractResolver
         {
-            return JsonConvert.SerializeObject(item);
+            NamingStrategy = new CamelCaseNamingStrategy()
+        };
+        
+        private static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            ContractResolver = ContractResolver,
+            Formatting = Formatting.Indented
+        };
+        
+        public static readonly JsonSerializerSettings DefaultCaseSettings = new JsonSerializerSettings
+        {
+            Formatting = Formatting.Indented
+        };
+        
+        public static string ToJson<T>(this T item, JsonSerializerSettings settings = null)
+        {
+            return JsonConvert.SerializeObject(item, settings??Settings);
         }
-
+        
         public static T FromJson<T>(this string data)
         {
             return JsonConvert.DeserializeObject<T>(data);
