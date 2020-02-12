ab4869a8ebf6fc2635848bb9756e903605a27c51
itchy2701 <hurst.richard@gmail.com>
2020-02-12 15:32:03 -0700

    made the cache timeout configurable

--- src/Xerris.DotNet.Core/Cache/WaitToFinishMemoryCache.cs
+++ src/Xerris.DotNet.Core/Cache/WaitToFinishMemoryCache.cs
@@ -8,45 +8,54 @@ namespace Xerris.DotNet.Core.Cache
 {
     public class WaitToFinishMemoryCache : ICache
     {
+        private readonly int slidingExpirationInMinutes;
+        private readonly int absoluteExpirationInMinutes;
         private readonly MemoryCache cache = new MemoryCache(new MemoryCacheOptions {SizeLimit = 100});
 
         private readonly ConcurrentDictionary<object, SemaphoreSlim> locks =
             new ConcurrentDictionary<object, SemaphoreSlim>();
 
+        public WaitToFinishMemoryCache(int slidingExpirationInMinutes = 2, int absoluteExpirationInMinutes = 15)
+        {
+            this.slidingExpirationInMinutes = slidingExpirationInMinutes;
+            this.absoluteExpirationInMinutes = absoluteExpirationInMinutes;
+        }
+
         public async Task<TItem> GetOrCreate<TItem>(object key, Func<Task<TItem>> createItem)
         {
-            if (cache.TryGetValue(key, out TItem cacheEntry)) return cacheEntry;
-            
-            var myLock = locks.GetOrAdd(key, k => new SemaphoreSlim(1, 1));
+            if (!cache.TryGetValue(key, out TItem cacheEntry)) // Look for cache key.
+            {
+                var myLock = locks.GetOrAdd(key, k => new SemaphoreSlim(1, 1));
 
-            await myLock.WaitAsync();
-            try
-            {
-                if (!cache.TryGetValue(key, out cacheEntry))
+                await myLock.WaitAsync();
+                try
                 {
-                    // Key not in cache, so get data.
-                    cacheEntry = await createItem();
-                    cache.Set(key, cacheEntry, CreateCacheOptions());
+                    if (!cache.TryGetValue(key, out cacheEntry))
+                    {
+                        // Key not in cache, so get data.
+                        cacheEntry = await createItem();
+                        cache.Set(key, cacheEntry, CreateCacheOptions());
+                    }
                 }
-            }
-            finally
-            {
-                myLock.Release();
+                finally
+                {
+                    myLock.Release();
+                }
             }
 
             return cacheEntry;
         }
 
-        private static MemoryCacheEntryOptions CreateCacheOptions()
+        private MemoryCacheEntryOptions CreateCacheOptions()
         {
             var cacheEntryOptions = new MemoryCacheEntryOptions()
                 .SetSize(1) //Size amount
                 //Priority on removing when reaching size limit (memory pressure)
                 .SetPriority(CacheItemPriority.High)
                 // Keep in cache for this time, reset time if accessed.
-                .SetSlidingExpiration(TimeSpan.FromHours(1))
+                .SetSlidingExpiration(TimeSpan.FromMinutes(slidingExpirationInMinutes))
                 // Remove from cache after this time, regardless of sliding expiration
-                .SetAbsoluteExpiration(TimeSpan.FromHours(6));
+                .SetAbsoluteExpiration(TimeSpan.FromMinutes(absoluteExpirationInMinutes));
             return cacheEntryOptions;
         }
     }
