a64f0d45663ca56c8fa27bd5d707881f442b5b0e
Greg Cook <greg@xerris.com>
2020-04-21 07:21:00 -0600

    additional clock unit tests for Utc time

--- test/Xerris.DotNet.Core.Test/Core/Time/FreezeClockTest.cs
+++ test/Xerris.DotNet.Core.Test/Core/Time/FreezeClockTest.cs
@@ -13,6 +13,22 @@ namespace Xerris.DotNet.Core.Test.Core.Time
         public FreezeClockTest()
         {
             Clock.Local.Thaw();
+            Clock.Utc.Thaw();
+        }
+
+        [Fact]
+        public void CanFreezeClockUtc()
+        {
+            DateTime then;
+            using (new FreezeClock())
+            {
+                then = Clock.Utc.Now;
+                Thread.Sleep(500);
+                Clock.Utc.Now.IsCloseEnough(then, .0001m);
+            }
+
+            Thread.Sleep(500);
+            Clock.Utc.Now.Should().NotBe(then);
         }
 
         [Fact]
@@ -44,10 +60,26 @@ namespace Xerris.DotNet.Core.Test.Core.Time
             Clock.Local.Now.IsCloseEnough(then, .0001m);
             
         }
+        
+        [Fact]
+        public void CanFreezeClockForSpecificDateTimeUtc()
+        {
+            var then = new DateTime(2018, 01, 15);
+            using (new FreezeClock(then))
+            {
+                Clock.Utc.Now.IsCloseEnough(then, .0001m);
+                Thread.Sleep(500);
+                Clock.Utc.Now.IsCloseEnough(then, .0001m);
+            }
+            Thread.Sleep(500);
+            Clock.Utc.Now.IsCloseEnough(then, .0001m);
+            
+        }
 
         public void Dispose()
         {
             Clock.Local.Thaw();
+            Clock.Utc.Thaw();
         }
     }
 }
