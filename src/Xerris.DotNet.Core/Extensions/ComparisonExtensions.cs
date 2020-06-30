6f4b306e7c5bfd793bc08d8b4c3a52d0b9f2e1b1
Greg <greg@xerris.com>
2020-06-30 09:15:49 -0600

    force update

--- src/Xerris.DotNet.Core/Extensions/ComparisonExtensions.cs
+++ src/Xerris.DotNet.Core/Extensions/ComparisonExtensions.cs
@@ -4,9 +4,7 @@ namespace Xerris.DotNet.Core.Extensions
 {
     public static class ComparisonExtensions
     {
-        public static bool IsCloseEnough<T>(this T left, T right, decimal tolerance) where T : IComparable<T>
-        {
-            return Math.Abs(left.CompareTo(right)) <= tolerance;
-        }
+        public static bool IsCloseEnough<T>(this T left, T right, decimal tolerance) where T : IComparable<T> =>
+            Math.Abs(left.CompareTo(right)) <= tolerance;
     }
 }
