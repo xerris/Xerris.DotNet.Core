5b4d9e0595d34e6be0616cfac95c45ccf7a643cb
Greg Cook <greg@xerris.com>
2021-05-07 06:13:59 -0600

    actual run to test Push to nuget via CI/CD

--- gulpfile.js
+++ gulpfile.js
@@ -70,9 +70,9 @@ async function _push() {
     var cmd = `dotnet nuget push ./dist/${packageName}.${version}.nupkg`;
     var nugetApi = `-k ${process.env.NUGET_APIKEY}`
     var execCmd = `${cmd} ${nugetApiUrl} ${nugetApi}`
-    // exec(execCmd).on('exit', () => {
-    //     console.log(`pushed to nuget for verion: ${version}`);
-    // });
+    exec(execCmd).on('exit', () => {
+        console.log(`pushed to nuget for verion: ${version}`);
+    });
 }
 
 exports.Version = gulp.series(_clean, _version);
