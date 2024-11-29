7629c6e1a1d20509c10c89ed2e848579d85d3513
Greg Cook <greg.cook@nutrien.com>
2024-11-29 07:46:48 -0700

    xer-1 - using the common build from xerris-common-actions

--- sample/Xerris.Sample.Api/Program.cs
+++ sample/Xerris.Sample.Api/Program.cs
@@ -1,6 +1,3 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Xerris.DotNet.Core;
 using Xerris.Sample.Api.Services;
 
